f5cf719aebc9793c5a4421679a2979d6129fc434
unknown <ahamedraju152@gmail.com>
2025-10-14 01:20:09 +0600

    user can delete her/his recipe from both UI and server

--- src/Component/MyRecipe.jsx
+++ src/Component/MyRecipe.jsx
@@ -1,6 +1,7 @@
 import React, { use, useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import { AuthContext } from '../Context/AuthContext';
+import Swal from 'sweetalert2';
 
 
 const MyRecipe = () => {
@@ -45,7 +46,39 @@ const MyRecipe = () => {
 
     }
 
-    
+    const handleDeleteRecipe = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/recipes/${id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount) {
+                            setRecipes((prev) => prev.filter((r) => r._id !== id));
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your recipe has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+            }
+        });
+    }
+
+
+
     return (
         <>
             <Navbar />
@@ -74,7 +107,8 @@ const MyRecipe = () => {
                                             setIsModalOpen(true);
                                         }} className="btn bg-gray-300">update</button>
 
-                                        <button className='btn bg-red-400'>
+                                        <button onClick={
+                                            () => handleDeleteRecipe(recipe._id)} className='btn bg-red-400'>
                                             delete
                                         </button>
                                     </div>
