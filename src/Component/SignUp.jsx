bd0816719955eb5d2b2bff892ce476ceedd996dc
unknown <ahamedraju152@gmail.com>
2025-10-15 03:07:48 +0600

    added a google auth for signUp page and navigate to user choose path default home

--- src/Component/SignUp.jsx
+++ src/Component/SignUp.jsx
@@ -2,10 +2,11 @@ import React, { use } from 'react';
 import { AuthContext } from '../Context/AuthContext';
 import Navbar from './Navbar';
 import { Link, useLocation, useNavigate } from 'react-router';
+import { FcGoogle } from "react-icons/fc";
 
 
 const SignUp = () => {
-    const { createUser } = use(AuthContext);
+    const { createUser, googleSignIn } = use(AuthContext);
     const location = useLocation();
     const navigate = useNavigate();
 
@@ -18,7 +19,7 @@ const SignUp = () => {
         console.log(userData);
 
         // create a user
-         createUser(userData.email, userData.password)
+        createUser(userData.email, userData.password)
             .then(result => {
                 console.log(result.user);
                 navigate(location?.state || '/');
@@ -29,9 +30,21 @@ const SignUp = () => {
     }
 
 
+    const handleGoogleSignIn = () =>{
+            googleSignIn()
+            .then(result => {
+                console.log(result)
+                navigate(location?. state || '/');
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className="card bg-base-100 mx-auto mt-14 max-w-sm shrink-0 shadow-2xl">
                 <div className="card-body">
                     <h1 className="text-3xl text-center font-bold"> Please Register </h1>
@@ -41,10 +54,14 @@ const SignUp = () => {
                         <label className="label">Email</label>
                         <input type="email" name='email' className="input" placeholder="Email" />
                         <label className="label">Password</label>
-                        <input type="password" name='password' className="input"  placeholder="Password" />
+                        <input type="password" name='password' className="input" placeholder="Password" />
                         <div><a className="link link-hover">Forgot password?</a></div>
                         <button className="btn btn-neutral mt-4">Sign Up</button>
                         <p>Already have an Account? Please <Link to="/signIn" className='text-blue-500 underline font-bold'>Login</Link></p>
+
+                        <button onClick={handleGoogleSignIn} className='btn bg-white text-black border-[#e5e5e5]'>
+                            <FcGoogle />Login with Google
+                        </button>
                     </form>
                 </div>
             </div>
